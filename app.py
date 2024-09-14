6a02e16b2111506530019e5734a6a8cfdf445f8a
faresomee <155561314+faresomee@users.noreply.github.com>
2024-09-14 19:15:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 from PIL import Image
 import io
 
 app = Flask(__name__)
 
-# Load the model (replace 'path_to_your_model.h5' with the actual path to your model)
-model = tf.keras.models.load_model('my_model.h5')
+# Load the quantized model
+interpreter = tf.lite.Interpreter(model_path="model_quantized.tflite")
+interpreter.allocate_tensors()
+
+# Get model input and output details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
 # Prediction function
-def predict_from_model(model, img_array):
-    predictions = model.predict(img_array)
+def predict_from_model(img_array):
+    # Preprocess image array if needed (e.g., normalize)
+    img_array = img_array / 255.0  # Normalize if required by the model
+    interpreter.set_tensor(input_details[0]['index'], img_array)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]['index'])
     predicted_class = np.argmax(predictions, axis=1)
     return int(predicted_class[0])
 
@@ -30,10 +39,10 @@ def predict():
         img = Image.open(io.BytesIO(file.read()))
         img = img.resize((128, 128))  # Resize image as required by your model
         img_array = np.array(img)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array = img_array / 255.0  # Normalize image array
-
-        prediction = predict_from_model(model, img_array)
+        img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+        
+        # Make prediction
+        prediction = predict_from_model(img_array)
         
         response = {
             "predicted_class": prediction,  # Adjust based on your model's output
